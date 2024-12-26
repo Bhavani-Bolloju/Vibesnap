5539c90e3a17f48124d554c484a56fa4d5639038
Bhavani Bolloju <bhavani.bolloju@gmail.com>
2024-12-26 14:39:47 +0530

    configured package.json file

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 
 import { ToastContainer, Slide } from "react-toastify";
 
@@ -8,6 +7,7 @@ import { BrowserRouter } from "react-router";
 import AuthProvider from "./firebase/auth/auth-provider";
 // import AppRoutes from "./routes/app-routes";
 import AppPage from "./App";
+import "@/styles/app.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
